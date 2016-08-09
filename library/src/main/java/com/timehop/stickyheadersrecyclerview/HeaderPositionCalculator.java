aef207f483c2861a08fc7832f983e661314e95ed
squeezymo <squeezymo@gmail.com>
2016-08-09 16:31:24 +0300

    Some fixes

--- library/src/main/java/com/timehop/stickyheadersrecyclerview/HeaderPositionCalculator.java
+++ library/src/main/java/com/timehop/stickyheadersrecyclerview/HeaderPositionCalculator.java
@@ -49,13 +49,20 @@ public class HeaderPositionCalculator {
    */
   public boolean hasStickyHeader(View itemView, int orientation, int position) {
     int offset, margin;
+
     mDimensionCalculator.initMargins(mTempRect1, itemView);
-    if (orientation == LinearLayout.VERTICAL) {
-      offset = itemView.getTop();
-      margin = mTempRect1.top;
+
+    if (itemView != null) {
+      if (orientation == LinearLayout.VERTICAL) {
+        offset = itemView.getTop();
+        margin = mTempRect1.top;
+      } else {
+        offset = itemView.getLeft();
+        margin = mTempRect1.left;
+      }
     } else {
-      offset = itemView.getLeft();
-      margin = mTempRect1.left;
+      offset = -1;
+      margin = 0;
     }
 
     return offset <= margin && mAdapter.getHeaderId(position) >= 0;
@@ -109,31 +116,34 @@ public class HeaderPositionCalculator {
 
   private void initDefaultHeaderOffset(Rect headerMargins, RecyclerView recyclerView, View header, View firstView, int orientation) {
     int translationX, translationY;
+
     mDimensionCalculator.initMargins(mTempRect1, header);
 
-    ViewGroup.LayoutParams layoutParams = firstView.getLayoutParams();
-    int leftMargin = 0;
-    int topMargin = 0;
-    if (layoutParams instanceof ViewGroup.MarginLayoutParams) {
-      ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) layoutParams;
-      leftMargin = marginLayoutParams.leftMargin;
-      topMargin = marginLayoutParams.topMargin;
+    if (header != null && firstView != null) {
+      ViewGroup.LayoutParams layoutParams = firstView.getLayoutParams();
+      int leftMargin = 0;
+      int topMargin = 0;
+      if (layoutParams instanceof ViewGroup.MarginLayoutParams) {
+        ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) layoutParams;
+        leftMargin = marginLayoutParams.leftMargin;
+        topMargin = marginLayoutParams.topMargin;
+      }
+
+      if (orientation == LinearLayoutManager.VERTICAL) {
+        translationX = firstView.getLeft() - leftMargin + mTempRect1.left;
+        translationY = enableStickyHeader ?
+                Math.max(firstView.getTop() - topMargin - header.getHeight() - mTempRect1.bottom, getListTop(recyclerView) + mTempRect1.top) :
+                firstView.getTop() - topMargin - header.getHeight() - mTempRect1.bottom;
+      } else {
+        translationY = firstView.getTop() - topMargin + mTempRect1.top;
+        translationX = enableStickyHeader ?
+                Math.max(firstView.getLeft() - leftMargin - header.getWidth() - mTempRect1.right, getListLeft(recyclerView) + mTempRect1.left) :
+                firstView.getLeft() - leftMargin - header.getWidth() - mTempRect1.right;
+      }
+
+      headerMargins.set(translationX, translationY, translationX + header.getWidth(), translationY + header.getHeight());
     }
 
-    if (orientation == LinearLayoutManager.VERTICAL) {
-      translationX = firstView.getLeft() - leftMargin + mTempRect1.left;
-      translationY = Math.max(
-          firstView.getTop() - topMargin - header.getHeight() - mTempRect1.bottom,
-          getListTop(recyclerView) + mTempRect1.top);
-    } else {
-      translationY = firstView.getTop() - topMargin + mTempRect1.top;
-      translationX = Math.max(
-          firstView.getLeft() - leftMargin - header.getWidth() - mTempRect1.right,
-          getListLeft(recyclerView) + mTempRect1.left);
-    }
-
-    headerMargins.set(translationX, translationY, translationX + header.getWidth(),
-            translationY + header.getHeight());
   }
 
   private boolean isStickyHeaderBeingPushedOffscreen(RecyclerView recyclerView, View stickyHeader) {
--- library/src/main/java/com/timehop/stickyheadersrecyclerview/caching/HeaderViewCache.java
+++ library/src/main/java/com/timehop/stickyheadersrecyclerview/caching/HeaderViewCache.java
@@ -18,8 +18,7 @@ public class HeaderViewCache implements HeaderProvider {
   private final LongSparseArray<View> mHeaderViews = new LongSparseArray<>();
   private final OrientationProvider mOrientationProvider;
 
-  public HeaderViewCache(StickyRecyclerHeadersAdapter adapter,
-      OrientationProvider orientationProvider) {
+  public HeaderViewCache(StickyRecyclerHeadersAdapter adapter, OrientationProvider orientationProvider) {
     mAdapter = adapter;
     mOrientationProvider = orientationProvider;
   }
@@ -29,14 +28,13 @@ public class HeaderViewCache implements HeaderProvider {
     long headerId = mAdapter.getHeaderId(position);
 
     View header = mHeaderViews.get(headerId);
+
     if (header == null) {
-      //TODO - recycle views
-      RecyclerView.ViewHolder viewHolder = mAdapter.onCreateHeaderViewHolder(parent);
+      RecyclerView.ViewHolder viewHolder = mAdapter.onCreateHeaderViewHolder(parent, position);
       mAdapter.onBindHeaderViewHolder(viewHolder, position);
       header = viewHolder.itemView;
       if (header.getLayoutParams() == null) {
-        header.setLayoutParams(new ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        header.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
       }
 
       int widthSpec;
@@ -50,14 +48,15 @@ public class HeaderViewCache implements HeaderProvider {
         heightSpec = View.MeasureSpec.makeMeasureSpec(parent.getHeight(), View.MeasureSpec.EXACTLY);
       }
 
-      int childWidth = ViewGroup.getChildMeasureSpec(widthSpec,
-          parent.getPaddingLeft() + parent.getPaddingRight(), header.getLayoutParams().width);
-      int childHeight = ViewGroup.getChildMeasureSpec(heightSpec,
-          parent.getPaddingTop() + parent.getPaddingBottom(), header.getLayoutParams().height);
+      int childWidth = ViewGroup.getChildMeasureSpec(widthSpec, parent.getPaddingLeft() + parent.getPaddingRight(), header.getLayoutParams().width);
+      int childHeight = ViewGroup.getChildMeasureSpec(heightSpec, parent.getPaddingTop() + parent.getPaddingBottom(), header.getLayoutParams().height);
+
       header.measure(childWidth, childHeight);
       header.layout(0, 0, header.getMeasuredWidth(), header.getMeasuredHeight());
+
       mHeaderViews.put(headerId, header);
     }
+
     return header;
   }
 
